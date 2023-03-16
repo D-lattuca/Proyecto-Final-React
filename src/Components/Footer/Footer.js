6c95a3a907ab5c9dc282f5c5ac7c463da37d38d2
Dante <dantetutuca@gmail.com>
2023-03-16 11:42:28 -0300

    Proyecto Final

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="text-center p-3 bg-light footer container-fluid">
+      2023 - Dante Lattuca | 
+      <a  href="" target="_blank" rel="noreferrer">
+       github
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
